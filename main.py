67b58cdf6fa4f4e7194db9b11568bf69f2359605
bobbyhadz <opmftw@gmail.com>
2024-04-09 12:23:31 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Creating a Tuple from user Input in Python
+
+import ast
+
+# ✅ create a tuple from string user input
+
+my_tuple = tuple(input('Enter space-separated words: ').split())
+
+print(my_tuple)
+
+# -------------------------------
+
+# ✅ create a tuple from integer user input
+
+user_input = input('Enter space-separated integers: ')
+
+my_tuple = tuple(int(item) for item in user_input.split())
+
+print(my_tuple)
